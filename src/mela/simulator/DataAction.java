ddad8a22ba91a4a8ce2ed6c415311984330de54b
LudovicaLuisaVissat <ludovicaluisavissat@gmail.com>
2018-07-20 13:29:42 +0100

    First part on DataHandler

---
+++ src/mela/simulator/DataAction.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package mela.simulator;
+
+import java.util.ArrayList;
+
+/**
+ * @author ludovicaluisavissat
+ *
+ */
+public class DataAction implements DataHandler {
+	
+	private ArrayList<String> listNames;
+
+	@Override
+	public void start(int iterations) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Trajectory t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void commit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void setNames(ArrayList<String> names){
+		this.listNames = names;
+	}
+	
+}
---
+++ src/mela/simulator/DataAndMeta.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package mela.simulator;
+
+/**
+ * @author ludovicaluisavissat
+ *
+ */
+public class DataAndMeta implements DataHandler {
+
+	@Override
+	public void start(int iterations) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Trajectory t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void commit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
